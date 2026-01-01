73c9477b10be4f1c91f60b00e36d42981a5f7a60
ha1704840-ui <ha1704840@gmail.com>
2026-01-01 14:02:52 +0330

    Add files via upload

--- server.js
+++ server.js
@@ -126,7 +126,7 @@ app.delete("/api/contact/:id", (req, res) => {
 // --------------------------------------------
 
 // Server
-const PORT = process.env.PORT || 3000
+const PORT = process.env.port || 3000
 app.listen(PORT, () => {
   console.log("Server running on port" + PORT);
 });
